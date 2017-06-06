bff1e290bf24ceb3fdf5545adf26818390646761
freeeflyer <github.com@xoop.org>
2017-06-06 10:27:57 +0200

    remove password from log

--- ftpbackup.sh
+++ ftpbackup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source ./ftpbackup.ini
+CURLOPT="-s"
+LOGFILE="/var/log/ftpbackup.log"
+source ../etc/ftpbackup.ini
 if [ -z $FTPHOST ]
 then
     echo Missing config file ../etc/backup.ini
@@ -10,13 +12,19 @@ DATE=$(date +%Y%m%d)
 BACKUP_FAILED=
 HOSTNAME=$(hostname -A)
 
+function pipe2log()
+{
+   DATE=$(date --rfc-3339=second) 
+   cat | sed "s#^#${DATE}: #" >> $LOGFILE
+}
+
 function do_backup_fs()
 {
    dir=${1/\//} # remove leading "/"
    filename="${DATE}_${dir//\//_}.tgz"
    url="ftp://${FTPUSER}:${FTPPWD}@${FTPHOST}/${HOSTNAME/ /}/${filename}"
-   echo "Saving /$dir to $url"
-   tar czf - /${dir} | curl -T - ${url}
+   echo "Saving /$dir to ${FTPHOST}" | pipe2log
+   tar czf - /${dir} 2>/dev/null | curl $CURLOPT -T - ${url} | pipe2log
 }
 
 function test_backup_fs()
@@ -26,7 +34,7 @@ function test_backup_fs()
    url="ftp://${FTPUSER}:${FTPPWD}@${FTPHOST}/${HOSTNAME/ /}/${filename}"
    if ! curl ${url} | tar tzf - &> /dev/null; then
       BACKUP_FAILED="${BACKUP_FAILED} $dir"
-      echo backup failed : $url
+      echo backup failed : ${FTPHOST}
    fi
 }
 
@@ -35,8 +43,8 @@ function do_backup_mysql()
    base=${1}
    filename="${DATE}_mysql_${base}.sql.gz"
    url="ftp://${FTPUSER}:${FTPPWD}@${FTPHOST}/${HOSTNAME/ /}/${filename}"
-   echo "Saving mysql base $base to $url"
-   mysqldump --defaults-file=${MYSQLPASSFILE} ${base} | gzip | curl -T - ${url}
+   echo "Saving mysql base $base to ${FTPHOST}" | pipe2log
+   mysqldump --defaults-file=${MYSQLPASSFILE} ${base} | gzip | curl $CURLOPT -T - ${url} | pipe2log
 }
 
 function do_backup_pg()
@@ -44,9 +52,9 @@ function do_backup_pg()
    base=${1}
    filename="${DATE}_pgsql_${base}.sql.gz"
    url="ftp://${FTPUSER}:${FTPPWD}@${FTPHOST}/${HOSTNAME/ /}/${filename}"
-   echo "Saving pg base $base to $url"
-   sudo -u postgres pg_dump ${base} | gzip | curl -sS -T - ${url}
-   echo "Status : ${PIPESTATUS[*]}"
+   echo "Saving pg base $base to ${FTPHOST}" | pipe2log
+   sudo -u postgres pg_dump ${base} | gzip | curl $CURLOPT -T - ${url} | pipe2log
+   echo "Status : ${PIPESTATUS[*]}" | pipe2log
 }
 
 
